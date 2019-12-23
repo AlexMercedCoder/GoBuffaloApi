aa010d1dde638de64e839ca846e58adf13086e1f
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-23 12:39:49 -0500

    basics done

---
+++ actions/todoes_test.go
@@ -0,0 +1,28 @@
+package actions
+
+import (
+  "testing"
+
+  "github.com/stretchr/testify/require"
+)
+
+func (as *ActionSuite) Test_TodoesResource_List() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TodoesResource_Show() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TodoesResource_Create() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TodoesResource_Update() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_TodoesResource_Destroy() {
+  as.Fail("Not Implemented!")
+}
+
